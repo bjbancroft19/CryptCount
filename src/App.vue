ae5ea1fead104295497442befa032c350076798e
Ben Bancroft <bjbancroft19@gmail.com>
2018-01-16 23:44:33 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <div class="container header">
+        <div class="header-left">
+            <h1>CryptoCompare</h1>
+        </div>
+        <div class="header-right">
+            <a href="https://github.com/bjbancroft19/cryptocompare" target="_blank"><i class="fa fa-github" aria-hidden="true"></i>&nbsp;View on GitHub</a>
+        </div>
+    </div>
+
+    <div class="container opening">
+        <p>Top 100 Cryptocurrencies by Market Cap</p>
+    </div>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+.container {
+  width: 95%;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.header {
+    display: flex;
+    flex-direction: row;
+    .header-left, .header-right { width: 50%; }
+    h1 {
+      text-align: left;
+      margin-top: 10px;
+      font-size: 1.6em;
+      letter-spacing: 5px;
+    }
+    a {
+      display: block;
+      color: #fff;
+      text-decoration: none;
+      text-align: right;
+      margin-top: 10px;
+    }
+}
+.opening {
+    text-align: center;
+    padding: 50px 0;
+}
+</style>
+
---
+++ src/components/Table.vue
@@ -0,0 +1,148 @@
+<template>
+    <table class="table table-hover">
+      <thead>
+          <tr>
+              <td>Rank</td>
+              <td>Name</td>
+              <td>Symbol</td>
+              <td>Price (USD)</td>
+              <td>1H</td>
+              <td>1D</td>
+              <td>1W</td>
+              <td>Market Cap (USD)</td>
+          </tr>
+      </thead>
+      <tbody>
+          <tr v-for="coin in coins" :key="coin.id">
+              <td>{{ coin.rank }}</td>
+              <td><img v-bind:src="getCoinImage(coin.symbol)"> &nbsp;{{ coin.name }}</td>
+              <td>{{ coin.symbol }}</td>
+              <td>{{ coin.price_usd | currency }}</td>
+              <td v-bind:style="getColor(coin.percent_change_1h)">
+                  <span v-if="coin.percent_change_1h > 0">+</span>{{ coin.percent_change_1h }}%
+              </td>
+              <td v-bind:style="getColor(coin.percent_change_1h)">
+                  <span v-if="coin.percent_change_24h > 0">+</span>{{ coin.percent_change_24h }}%
+              </td>
+              <td v-bind:style="getColor(coin.percent_change_1h)">
+                  <span v-if="coin.percent_change_7d > 0">+</span>{{ coin.percent_change_7d }}%
+              </td>
+              <td>{{ coin.market_cap_usd | currency }}</td>
+          </tr>
+      </tbody>
+  </table>
+</template>
+
+<script>
+import axios from 'axios';
+
+// The API for metadata about each currency, used for images. https://min-api.cryptocompare.com/data/
+let cryptocompareApi = "https://min-api.cryptocompare.com";
+let cryptocompareBaseImage = "https://www.cryptocompare.com";
+
+// The API for currency prices. https://coinmarketcap.com/api/
+let coinmarketcapApi = "https://api.coinmarketcap.com";
+
+// Send request every x milliseconds
+let updateInterval = 60 * 1000;
+
+export default {
+  name: 'Coins',
+
+  data() {
+      return {
+          coins: [],
+          coinData: {}
+      }
+  },
+
+  methods: {
+      
+      /**
+         * Use this data for getting the symbol which we can then use later on
+         * to get the logo.
+         */
+        getCoinData: function() {
+            let self = this;
+
+            axios.get(cryptocompareApi + "/data/all/coinlist")
+                .then((resp) => {
+                    this.coinData = resp.data.Data;
+                    this.getCoins();
+                })
+                .catch((err) => {
+                    this.getCoins();
+                    console.error(err);
+                })
+        },
+
+        /**
+         * Get the top 10 cryptocurrencies by value. The endpoint refreshes every 5 
+         * minutes.
+         */
+        getCoins: function() {
+            let self = this;
+
+            axios.get(coinmarketcapApi + "/v1/ticker/")
+                .then((resp) => {
+                    this.coins = resp.data;
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        },
+
+        /**
+         * Return the currency's logo using the symbol (e.g. 'ETH')
+         */
+        getCoinImage: function(symbol) {
+            if (this.coinData[symbol]) {
+                return cryptocompareBaseImage + this.coinData[symbol].ImageUrl;
+            }
+        },
+
+        /**
+         * Colour red or green for negative or positive values
+         */
+        getColor: (num) => {
+            return num > 0 ? "color: #00d174" : "color: #fb2853";
+        }
+
+  },
+
+  created() {
+      this.getCoinData()
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  table {
+    width: 100%;
+    color: #fff;
+    tr {
+      line-height: 50px;
+      &:nth-child(2n) {
+        background-color: rgba(255,255,255,0.1);
+      }
+      td {
+        padding: 10px;
+        &:first-child { padding-left: 20px; }
+        img {
+          width: 25px;
+          vertical-align: middle;
+        }
+      }
+    }
+    tbody {
+      tr {
+        cursor: pointer;
+        &:hover {
+          background-color: rgba(255,255,255,0.3);
+        }
+      }
+    }
+  }
+</style>
