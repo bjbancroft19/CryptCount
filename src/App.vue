5a81801fba0e34fdb321019b991aded963ca0a60
Ben Bancroft <bjbancroft19@gmail.com>
2018-01-18 22:48:50 +0000

    Added header links and styled

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div id="app">
   <div class="container header">
     <div class="header-left">
-      <h1>CryptoCompare</h1>
+      <router-link tag="h1" to="/">CryptoCompare</router-link>
     </div>
     <div class="header-right">
-      <a href="https://github.com/bjbancroft19/cryptocompare" target="_blank"><i class="fa fa-github" aria-hidden="true"></i>&nbsp;View on GitHub</a>
+      <a href="https://github.com/bjbancroft19/cryptocompare" target="_blank"><icon name="github"></icon> &nbsp;View on GitHub</a>
     </div>
   </div>
   <router-view/>
@@ -13,8 +13,14 @@
 </template>
 
 <script>
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Icon
+  }
 }
 </script>
 
@@ -36,12 +42,18 @@ export default {
       @media screen and (max-width: 1000px) {
         display: none;
       }
+      .fa-icon {
+        height: 1.2em;
+        vertical-align: sub;
+        width: auto;
+      }
     }
     h1 {
       text-align: left;
       margin-top: 10px;
       font-size: 1.6em;
       letter-spacing: 5px;
+      cursor: pointer;
       @media screen and (max-width: 1000px) {
         text-align: center;
       }
