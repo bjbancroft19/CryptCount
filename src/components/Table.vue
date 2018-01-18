bdecb2c14b117ccc16d3662bc96ef85015032494
Ben Bancroft <bjbancroft19@gmail.com>
2018-01-18 21:19:52 +0000

    Added circulating supply

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -13,9 +13,8 @@
         <td>Name</td>
         <td>Symbol</td>
         <td>Price (USD)</td>
-        <td>1H</td>
-        <td>1D</td>
-        <td>1W</td>
+        <td>24h</td>
+        <td>Circulating Supply</td>
         <td>Market Cap (USD)</td>
       </tr>
     </thead>
@@ -26,15 +25,10 @@
         <td><img v-bind:src="getCoinImage(coin.symbol)"> &nbsp;{{ coin.name }}</td>
         <td>{{ coin.symbol }}</td>
         <td>{{ coin.price_usd | currency }}</td>
-        <td v-bind:style="getColor(coin.percent_change_1h)">
-          <span v-if="coin.percent_change_1h > 0">+</span>{{ coin.percent_change_1h }}%
-        </td>
         <td v-bind:style="getColor(coin.percent_change_24h)">
           <span v-if="coin.percent_change_24h > 0">+</span>{{ coin.percent_change_24h }}%
         </td>
-        <td v-bind:style="getColor(coin.percent_change_7d)">
-          <span v-if="coin.percent_change_7d > 0">+</span>{{ coin.percent_change_7d }}%
-        </td>
+        <td>{{ coin.available_supply | currency('', 0) }} {{ coin.symbol }}</td>
         <td>{{ coin.market_cap_usd | currency }}</td>
       </router-link>
     </tbody>
